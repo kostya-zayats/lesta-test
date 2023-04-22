ded918126f83adae3a37219e3ee2c56ae62ad227
kanstantsin.zayats <kanstantsin.zayats@altoros.com>
2023-04-22 15:56:13 +0300

    preload

--- script/dev/initModel.js
+++ script/dev/initModel.js
@@ -2,7 +2,7 @@ var iframe = document.getElementById( 'api-frame' );
 var uid = '7c5b98b1b57841aba3048df45a13b8ea';
 
 // By default, the latest version of the viewer API will be used.
-var client = new Sketchfab( iframe );
+let client = new Sketchfab( iframe );
 
 // Alternatively, you can request a specific version.
 // var client = new Sketchfab( '1.12.1', iframe );
@@ -14,11 +14,16 @@ client.init( uid, {
 
       // API is ready to use
       // Insert your code here
+      document.getElementById('preload').classList.add('hidden');
       console.log( 'Viewer is ready' );
 
     } );
   },
   error: function onError() {
     console.log( 'Viewer error' );
-  }
+  },
+  autostart: 1,
+  ui_stop: 0,
+  preload: 1,
+  camera: 0
 } );
