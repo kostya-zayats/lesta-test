a5c1711d6cc7d96bc4e86a6b0a0c6eb1a4b7ff9a
kanstantsin.zayats <kanstantsin.zayats@altoros.com>
2023-04-27 20:39:42 +0300

    add mouse look rotation

--- script/dev/initModel.js
+++ script/dev/initModel.js
@@ -1,29 +1,50 @@
-var iframe = document.getElementById( 'api-frame' );
-var uid = '7c5b98b1b57841aba3048df45a13b8ea';
+let version = '1.12.1';
+let iframe = document.getElementById('api-frame');
+let uid = '7c5b98b1b57841aba3048df45a13b8ea';
+let eventCatcher = document.createElement('div');
+eventCatcher.style.width = '100%';
+eventCatcher.style.height = '100%';
+eventCatcher.style.position = 'absolute';
+iframe.parentNode.insertBefore(eventCatcher, iframe);
+let client = new window.Sketchfab(version, iframe);
 
-// By default, the latest version of the viewer API will be used.
-let client = new Sketchfab( iframe );
+var success = function success(api) {
+  api.start(function () {
+    //Be carefull, the mouse can't be detected on the iframe
+    //If you want to fix this, you have to put an overlay on the iframe
+    document.body.addEventListener('mousemove', function (event) {
+      // Calculate the location of the middle of the frame (Where we want the model to stay)
+      let box = iframe.getBoundingClientRect();
+      let frameX = box.left + box.width / 2;
+      let frameY = box.top + box.height / 2;
+      let x = event.pageX - frameX;
+      let y = event.pageY - frameY;
+      let z = 2;
 
-// Alternatively, you can request a specific version.
-// var client = new Sketchfab( '1.12.1', iframe );
+      // Calculate the distance, normalize the vecteur by divising by distance and multiplicate by a factor
+      let distance = Math.sqrt(Math.pow(x, 2) + Math.pow(y, 2) + Math.pow(z, 2));
+      x = x / distance * 15;
+      y = y / distance * 15;
+      z = z / distance * 15;
 
-client.init( uid, {
-  success: function onSuccess( api ){
-    api.start();
-    api.addEventListener( 'viewerready', function() {
+      api.getCameraLookAt(function(err, camera) {
+        window.console.log(camera.position); // [x, y, z]
+        window.console.log(camera.target); // [x, y, z]
+      });
 
-      // API is ready to use
-      // Insert your code here
-      document.getElementById('preload').classList.add('hidden');
-      console.log( 'Viewer is ready' );
-
-    } );
-  },
+      // The disposition of x, y and z depend on how the model was made
+      api.setCameraLookAt([y, -x, z], [0, -1.2, 0], 0);
+    });
+  });
+  api.addEventListener('viewerready', function() {
+    api.setCameraLookAt([0, -15, 0], [0, -1.2, 0], 0);
+    document.getElementById('preload').classList.add('hidden');
+  });
+};
+client.init(uid, {
+  success: success,
   error: function onError() {
-    console.log( 'Viewer error' );
+    console.log('Viewer error');
   },
-  autostart: 1,
-  ui_stop: 0,
-  preload: 1,
   camera: 0
-} );
+});
